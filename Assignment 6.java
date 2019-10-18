75a21bf5d6a4ce4ba085ed3b56d71313d689ba13
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2019-10-18 00:41:07 -0700

    Update Assignment 6.java

--- Assignment 6.java
+++ Assignment 6.java
@@ -36,11 +36,17 @@
 import java.util.Scanner;
 import java.lang.Math; 
 
+/*this assignment is pretty stupid but not that hard
+  once you find out that you have to use two for loops:
+     - one forward (flip)
+     - one backward (sake)                             */
+
 class Main {
   public static void main(String[] args)
   {
     Scanner scan = new Scanner (System.in);
     int length1 = -1;
+    //make the dummies who cant read input a correct length
     while(length1 < 10)
     {
       System.out.println("Enter an array length (must be 10 or greater)");
@@ -48,6 +54,7 @@ class Main {
     }
     int arr1 [] = new int [length1];
     int arr2 [] = new int [length1];
+    //creation of array 1
     for(int a = 0; a < length1; a++)
     {
       if(a == 0)
@@ -57,6 +64,7 @@ class Main {
       arr1[a] = (int)(Math.random() * 100 + 1);
       System.out.print(" " + arr1[a]);
     }
+    //creation of array 2
     for(int b = 0; b < length1; b++)
     {
       if(b == 0)
@@ -67,6 +75,7 @@ class Main {
       arr2[b] = (int)(Math.random() * 100 + 1);
       System.out.print(" " + arr2[b]);
     }
+    //merging the two arrays (mergeArr)
     int c  = length1 * 2; //2 arrays of the same length
     int count = 0;
     int z = 0;
@@ -78,6 +87,28 @@ class Main {
       count++;
       z+=2;
     }
+    //Take each number and start looping from behind
+    //in order to find repeats to rearrange the indexes forward
+    //and replace ending elements with zeros (dont question the names of my variables here)
+    for(int flip = 0; flip < c; flip++) //forwards loop
+    {
+      for(int sake = c - 1; sake > 0; sake--) //backwards loop
+      {
+        //make sure the the loop doesnt take out that flip value when sake overruns it
+        if((mergeArr[flip] == mergeArr[sake]) && (flip != sake)){
+          for(int cup = sake; cup < c; cup++)
+          {
+            if(cup == c - 1){
+              mergeArr[cup] = 0;
+            }
+            else{
+            mergeArr[cup] = mergeArr[cup+1];
+            }
+          }
+        }
+      }
+    }
+    //print the merged array BUT dont forget to exclude the zeros
     for(int shoot = 0; shoot < c; shoot++)
     {
       if(shoot == 0)
@@ -85,9 +116,9 @@ class Main {
         System.out.println("");
         System.out.print("Merged Array:");
       }
-      System.out.print(" " + mergeArr[shoot]);
+      if(mergeArr[shoot] != 0){
+        System.out.print(" " + mergeArr[shoot]);
+      }
     }
-    for(int flip = 0; flip < c; flip++)
-    {
   }
 }

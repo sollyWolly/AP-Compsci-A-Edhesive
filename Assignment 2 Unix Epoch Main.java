9d98aee2570320cf9987a2f733b67413f4048cfe
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2019-10-22 10:10:45 -0700

    Delete Assignment 2 Unix Epoch Main.java

--- Assignment 2 Unix Epoch Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-import java.lang.Math;
-
-class Main {
-    public static void main(String[] args) {
-      Scanner scan = new Scanner (System.in);
-      System.out.println("
-    }
-}

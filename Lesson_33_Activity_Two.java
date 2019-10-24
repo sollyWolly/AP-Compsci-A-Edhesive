4d073fa1f104a1d5c6c4d9c5f2b2150a0f8885b9
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2019-10-24 12:43:12 -0700

    Update Lesson_33_Activity_Two.java

--- Lesson_33_Activity_Two.java
+++ Lesson_33_Activity_Two.java
@@ -22,16 +22,17 @@ import java.util.Scanner;
 
 class Lesson_33_Activity_Two {
   
-    /*fill in the rest of randomize()
+    public static void randomize(int arr [])
     {
-    //Write your code here
+      for(int i = 0; i < arr.length; i++)
+      {
+        arr[i] = (int)(Math.random()*90 + 10);
+      }
     }
-    */
   
     public static void main(String[] args)
      {
-     /*
-      * Test your method here
-      */
+      int [] arrTest = new int [] {1,2,3,4,5,6,7,8,9};
+      randomize(arrTest);
     }
 }

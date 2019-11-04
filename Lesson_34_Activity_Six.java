3dda42bf945d61f871a03c0b405fda12e1715bb1
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2019-11-04 13:28:59 -0800

    Update Lesson_34_Activity_Six.java

--- Lesson_34_Activity_Six.java
+++ Lesson_34_Activity_Six.java
@@ -21,15 +21,26 @@ class Lesson_34_Activity_Six {
   
   public static boolean allPositive(int [] a)
     {
-    //Write your code here
-    //Change the return value as you see fit
-     return false; 
+    int count = 0;
+    for(int i = 0; i < a.length; i++)
+    {
+      if(a[i] >= 0)
+      {
+        count += 1;
+      }
+    }
+    if(count == a.length)
+    {
+      return true;
+    }
+    else{
+      return false;
+    }
     }
   
     public static void main(String[] args)
-     {
-     /*
-      * Test your method here
-      */
+    {
+     int [] b = new int [] {1, 2, 3, 1, 6, 7};
+     allPositive(b);
     }
 }

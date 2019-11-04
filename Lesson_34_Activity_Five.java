a5d36a7f36df54a268d73560fdafdc7510c710f6
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2019-11-04 13:10:23 -0800

    Update Lesson_34_Activity_Five.java

--- Lesson_34_Activity_Five.java
+++ Lesson_34_Activity_Five.java
@@ -21,15 +21,20 @@ class Lesson_34_Activity_Five {
   
    public static int sumEven(int [] a)
     {
-    //Write your code here
-    //Change the return value as you see fit
-     return 0; 
+    int total = 0;
+    for(int i = 0; i < a.length; i++)
+    {
+      if(a[i] % 2 == 0)
+      {
+        total += a[i];
+      }
+    }
+    return total;
     }
   
     public static void main(String[] args)
      {
-     /*
-      * Test your method here
-      */
+     int [] b = new int [] {1, 2, 3, 4, 5};
+     sumEven(b);
     }
 }

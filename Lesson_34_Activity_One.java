804d3790ab0f6df5bbd392d2b01e8fb14198425e
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2019-11-04 10:32:22 -0800

    Update Lesson_34_Activity_One.java

--- Lesson_34_Activity_One.java
+++ Lesson_34_Activity_One.java
@@ -19,17 +19,19 @@ import java.util.Scanner;
 
 class Lesson_34_Activity_One {
   
-   public static int sum(int [] a)
+   public static int sum(int [] ArrB)
+   {
+    int total = 0;
+    for(int i = 0; i < ArrB.length; i++)
     {
-    //Write your code here
-    //Change the return value as you see fit
-     return 0; 
+      total += ArrB[i];
     }
+     return total; 
+   }
   
     public static void main(String[] args)
-     {
-     /*
-      * Test your method here
-      */
+    {
+      int [] ArrA = new int [] {1, 2, 3};
+      sum(ArrA);
     }
 }

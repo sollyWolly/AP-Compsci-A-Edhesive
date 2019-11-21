d297970e557caa6a3aa14c859e79d2125c7e67ea
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2019-11-21 13:20:37 -0800

    Update T2L1 Activity one.java

--- T2L1 Activity one.java
+++ T2L1 Activity one.java
@@ -8,6 +8,19 @@ class Main {
       Scanner scan = new Scanner (System.in);
       ArrayList myList = new ArrayList();
       System.out.println("Please enter words, enter STOP to stop the loop.");
+      String word = scan.nextLine();
+      while(!word.equals("STOP"))
+      {
+          myList.add(word);
+          word = scan.nextLine();
+      }
+      System.out.println(myList.size());
+      System.out.println(myList);
+      if(myList.size() > 2)
+      {
+          myList.remove(0);
+          myList.remove(myList.size()-1);
+      }
+      System.out.println(myList);
     }
 }
-      

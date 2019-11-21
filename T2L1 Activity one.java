a5d93c0705d123ba2befc4ae002ec555f870989d
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2019-11-21 08:51:02 -0800

    Update T2L1 Activity one.java

--- T2L1 Activity one.java
+++ T2L1 Activity one.java
@@ -1,1 +1,13 @@
-import java.io.System.in;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Main {
+    
+    public static void main(String args[])
+    {
+      Scanner scan = new Scanner (System.in);
+      ArrayList myList = new ArrayList();
+      System.out.println("Please enter words, enter STOP to stop the loop.");
+    }
+}
+      

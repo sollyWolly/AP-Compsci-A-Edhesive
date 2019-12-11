481d60d51a9c917f3e6bbac21108a601c9221da4
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2019-12-11 13:40:04 -0800

    Update T2L2 Activity one.java

--- T2L2 Activity one.java
+++ T2L2 Activity one.java
@@ -3,6 +3,17 @@ import java.util.ArrayList;
 
 class Main{
   public static void main (Strings [] args){
-    
+    Scanner scan = new Scanner (System.in);
+    ArrayList <String> words = new ArrayList <String> ();
+    for(int i = 0; i < words.size(); i++)
+    {
+      words.add(scan.nextLine());
+    }
+    for(int i = 0; i < words.size(); i++)
+    {
+      for(int n = 0; words.get(n).size(); n++)
+      {
+        if(words.get(n).substring(0, 1) == "a")
+        {
   }
 }

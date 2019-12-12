6c3c7440d557e1cb39cd88b65f62b779c65fc331
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2019-12-12 13:29:32 -0800

    Update T2L2 Activity one.java

--- T2L2 Activity one.java
+++ T2L2 Activity one.java
@@ -2,18 +2,36 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 class Main{
-  public static void main (Strings [] args){
+  public static void main (String[] args){
     Scanner scan = new Scanner (System.in);
+    String userInput = " ";
+    String stop = "STOP";
+    System.out.println("Please enter words, enter STOP to stop the loop.");
     ArrayList <String> words = new ArrayList <String> ();
-    for(int i = 0; i < words.size(); i++)
+    while(!userInput.equals(stop))
     {
-      words.add(scan.nextLine());
+      userInput = scan.nextLine();
+      words.add(userInput);
     }
     for(int i = 0; i < words.size(); i++)
     {
-      for(int n = 0; words.get(n).size(); n++)
+      for(int n = 0; n < words.get(i).length(); n++)
       {
-        if(words.get(n).substring(0, 1) == "a")
+        if(n == words.get(i).length() - 1)
         {
+          if(words.get(i).substring(n) == "a")
+          {
+             System.out.println(words.get(i));
+          }
+        }
+        else
+        {
+           if(words.get(i).substring(n, n+1) == "a")
+           {
+              System.out.println(words.get(i));
+           }
+        }
+      }
+    }
   }
 }

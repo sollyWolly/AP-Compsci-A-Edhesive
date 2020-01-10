343f1c71b7696284cafad7f4d1f31688bf7b480f
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2020-01-10 13:42:56 -0800

    Update Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -4,29 +4,51 @@ public class Fraction
 {
   private int numerator;
   private int denominator;
+  private String mixed;
+  private String powfu = "";
   
   // TODO write default constructor
   public Fraction()
   {
+    numerator = 1;
+    denominator = 1;
   }
   
   // TODO write constructor for fraction n/d where n, d > 0
   public Fraction(int n, int d)
   {
+    if(n > 0){
+      numerator = n;
+    }
+    if(n <= 0){
+      numerator = 1;
+    }
+    if(d > 0){
+      denominator = d;
+    }
+    if(d <= 0){
+      denominator = 1;
+    }
   }
   
   // TODO write method to return fraction as a String
   public String toString()
   {
+    powfu = powfu + numerator + "/" + denominator;
+    return powfu;
   }
   
   // TODO write method to return fraction as a mixed number String
   public String mixedNumber()
   {
+    mixed = mixed + (numerator/denominator) + " " + (numerator-denominator) + "/" + denominator;
+    return mixed;
   }
   
   // TODO write method to add fraction n/d to this Fraction
   public void add(int n, int d)
   {
+    numerator = numerator * d + denominator * n;
+    denominator = 
   }
 }

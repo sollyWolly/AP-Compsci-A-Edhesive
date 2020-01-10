1676f3704c6ee39983f4688aef8dbbaa7b2c933d
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2020-01-10 13:19:14 -0800

    Add files via upload

---
+++ Fraction.java
@@ -0,0 +1,32 @@
+/* Term 2 Assignment 1 - Fraction */
+/* A class which is used to represent fractions*/
+public class Fraction
+{
+  private int numerator;
+  private int denominator;
+  
+  // TODO write default constructor
+  public Fraction()
+  {
+  }
+  
+  // TODO write constructor for fraction n/d where n, d > 0
+  public Fraction(int n, int d)
+  {
+  }
+  
+  // TODO write method to return fraction as a String
+  public String toString()
+  {
+  }
+  
+  // TODO write method to return fraction as a mixed number String
+  public String mixedNumber()
+  {
+  }
+  
+  // TODO write method to add fraction n/d to this Fraction
+  public void add(int n, int d)
+  {
+  }
+}

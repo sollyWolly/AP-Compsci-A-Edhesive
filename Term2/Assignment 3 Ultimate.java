dd12b3b29976457d7587a51f2f86f9cd0df282e8
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2020-02-07 13:34:12 -0800

    Update Assignment 3 Ultimate.java

--- Term2/Assignment 3 Ultimate.java
+++ Term2/Assignment 3 Ultimate.java
@@ -20,13 +20,13 @@ public class Person
 
 public class UltimatePlayer extends Person
 {
-  static int jerseyNumber = 0;
+  private int jerseyNumber = 0;
+  private static int num = 0;
   private String position;
   
   public UltimatePlayer(String firstName, String lastName, String pos)
   {
     super(firstName, lastName);
-    UltimatePlayer.jerseyNumber++;
     if(!pos.equals("handler") && !pos.equals("cutter"))
     {
       position = "handler";
@@ -35,6 +35,8 @@ public class UltimatePlayer extends Person
     {
       position = pos;
     }
+    num++;
+    jerseyNumber = num;
   }
   
   public String getPosition()
@@ -47,4 +49,48 @@ public class UltimatePlayer extends Person
     return (super.toString() + "\n   " + "Jersey #: " + jerseyNumber + "\n   " + "Position: " + position);
   }
 }
+
+public class Captain extends UltimatePlayer
+{
+  boolean type;
+  
+  public Captain(String firstName, String lastName, String position, boolean t)
+  {
+    super(firstName, lastName, position);
+    type = t;
+  }
+  
+  public String toString()
+  {
+    if(type)
+    {
+      return super.toString() + "\n   Captain: offense";
+    }
+    else
+    {
+      return super.toString() + "\n   Captain: defense";
+    }
+  }
+}
+
+public class Coach extends Person
+{
+  String role;
+  
+  public Coach(String firstName, String lastName, String r)
+  {
+    super(firstName, lastName);
+    role = r;
+  }
+  
+  public String toString()
+  {
+    return super.toString() + "\n   role: " + role;
+  }
+}
+
+
+
+
+
 

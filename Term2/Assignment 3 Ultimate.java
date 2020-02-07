0d2fd5a2e9910e22eacc1d3c29d03f1c4bdf7038
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2020-02-06 19:42:25 -0800

    Update Assignment 3 Ultimate.java

--- Term2/Assignment 3 Ultimate.java
+++ Term2/Assignment 3 Ultimate.java
@@ -1,3 +1,4 @@
+//Person Class Submission
 public class Person
 {
   private String firstname;
@@ -14,3 +15,35 @@ public class Person
     return (lastname + ", " + firstname);
   }
 }
+
+//UltimatePlayer Class Submission
+
+public class UltimatePlayer extends Person
+{
+  static int jerseyNumber;
+  private String position;
+  
+  public UltimatePlayer(String firstName, String lastName, String pos)
+  {
+    super(firstName, lastName);
+    if(!pos.equals("handler") && !pos.equals("cutter"))
+    {
+      position = "handler";
+    }
+    else
+    {
+      position = pos;
+    }
+  }
+  
+  public String getPosition()
+  {
+    return position;
+  }
+  
+  public String toString()
+  {
+    return (Person.lastname + ", " + Person.firstname + "\n   " + "Jersey #: " + jerseyNumber + "\n   " + "Position: " + position);
+  }
+}
+

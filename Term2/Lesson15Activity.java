1c299817788ee5b089e687ae950be2b1cba13cf4
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2020-03-19 15:11:51 -0700

    Update Lesson15Activity.java

--- Term2/Lesson15Activity.java
+++ Term2/Lesson15Activity.java
@@ -3,9 +3,31 @@ public class Lesson_15_Activity
   public static void sortAndPrint(String [] list)
   {
     String [] party = list;
+    String temp;
     for(int i = 0; i < party.length; i++)
     {
-      if(party[i].compareTo(party[i+1])
+      for(int k = i; k < party.length; k++)
+      {
+          if(party[k].compareTo(party[i]) < 0)
+          {
+              temp = party[i];
+              party[i] = party[k];
+              party[k] = temp;
+          }
+      }
     }
+    for(int i = 0; i < party.length; i++)
+    {
+      System.out.print(party[i]);
+      if(i != party.length -1)
+      {
+        System.out.print(" ");
+      }
+    }
+  }
+  public static void main(String [] args)
+  {
+      String [] hi = {"against" , "forms" , "belief" , "government" , "democratic" , "movement" , "understanding"};
+      sortAndPrint(hi);
   }
 }

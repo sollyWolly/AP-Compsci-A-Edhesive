54ec78df27211cf2ffa05bcbd94ab3a63196100b
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2020-03-19 14:43:46 -0700

    Create Lesson15Activity.java

---
+++ Term2/Lesson15Activity.java
@@ -0,0 +1,11 @@
+public class Lesson_15_Activity
+{
+  public static void sortAndPrint(String [] list)
+  {
+    String [] party = list;
+    for(int i = 0; i < party.length; i++)
+    {
+      if(party[i].compareTo(party[i+1])
+    }
+  }
+}

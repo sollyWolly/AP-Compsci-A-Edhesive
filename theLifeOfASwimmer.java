446a78b07765b593d3156cf3173f6814f3dc2691
sollyWolly <55569594+sollyWolly@users.noreply.github.com>
2020-05-19 14:10:07 -0700

    Update theLifeOfASwimmer.java

--- theLifeOfASwimmer.java
+++ theLifeOfASwimmer.java
@@ -58,20 +58,124 @@ public class Main
     //Pathways
     if(in == 1)
     {
-      System.out.println("The captain has been chosen");
+      System.out.println("You walk up to the captain. He asks \"What do you want?\"");
+      System.out.println("1 - \"Which events am I swimming?\"");
+      System.out.println("2 - \"Can I be on varsity?\"");
+      System.out.println("3 - \"You are honestly so bad at swimming.\"");
+      in = scan.nextInt();
+      if(in == 1)
+      {
+        System.out.println("He says \"Go look at the psych sheet you moron\"");
+        System.out.println("1 - Go look at the psych sheet");
+        System.out.println("2 - Demand to know the events from him");
+        in = scan.nextInt();
+        if(in == 1)
+        {
+          System.out.println("You look at the psych sheet. You don't see your name. You realize that the coach didn't put you in any events. You go home and eat ice cream.");
+        }
+        else if(in == 2)
+        {
+          System.out.println("He says \"Alright if you're going to be rude about it, then I'm going to kick you off the team. Goodbye.\"");
+        }
+      }
+      else if(in == 2)
+      {
+        System.out.println("He says \"Of course you can't. You're literally the worst swimmer here. My grandma can dive better than you.\"");
+        System.out.println("1 - \"Let's race then.\"");
+        in = scan.nextInt();
+        if(in == 1)
+        {
+          System.out.println("He agrees and you guys start to race. You win with a minute lead. He gets really mad and decides to quit. Coach makes you the new captain.");
+        }
+      }
+      else if(in == 3)
+      {
+        System.out.println("He says \"DO YOU KNOW WHO YOU'RE TALKING TO??!! There's a reason the coach made me captain.\"");
+        System.out.println("1 - \"Yeah I bet it's because he felt sorry for you.\"");
+        System.out.println("2 - \"Yeah well the coach might not be so bright then\"");
+        in = scan.nextInt();
+        if(in == 1)
+        {
+          System.out.println("He says \"Oh that's it, I'm asking coach if that's true. COACH IS THAT TRUE?\" Coach says \"Yes, you are only captain because I felt sorry for you\"");
+        }
+        else(in == 2)
+        {
+          System.out.println("He says \"COACH, THE NEW GUY SAYS YOU'RE STUPID.\" You get kicked off the team.");
+        }
+      }
     }
     else if(in == 2)
     {
-      System.out.println("The casual swimmers have been chosen");
+      System.out.println("You walk up to the casual swimmers. They are playing Smash on the Nintendo Switch.");
+      System.out.println("1 - \"Can I join?\"");
+      System.out.println("2 - \"Don't you guys think Smash is bad game.\"");
+      System.out.println("3 - \"YOU GUYS MISSED YOUR EVENTS!!!\"");
+      in = scan.nextInt();
+      if(in == 1)
+      {
+        System.out.println("They ignore you. You take the switch and throw it into the water. They all start beating you up.");
+      }
+      else if(in == 2)
+      {
+        System.out.println("They all stop playing the game and without saying any words, they start beating you up.");
+      }
+      else if(in == 3)
+      {
+        System.out.println("They all start freaking out and running over to the starting blocks. You finesse their Switch and go home.");
+      }
     }
     else if(in == 3)
     {
-      System.out.println("The competitive athletes have been chosen");
-    }
-    
-                       
-                       
-                       
+      System.out.println("You walk over to the competitive swimmers. They are all wearing headphones, so they can't hear you. You try to get the attention of one of them.");
+      System.out.println("1 - Snap in front of him");
+      System.out.println("2 - Rip off his headphones.");
+      System.out.println("3 - Beat him up");
+      in = scan.nextInt();
+      if(in == 1)
+      {
+        System.out.println("He says \"Hey what's up bud.\"");
+        System.out.println("1 - \"You smell like garbage.\"");
+        System.out.println("2 - \"Do you want to be friends?\"");
+        System.out.println("3 - \"Do you want the other half of my corndog?\"");
+        in = scan.nextInt();
+        if(in == 1)
+        {
+          System.out.println("He says \"Takes one to know one buddy.\" You are so traumatized by that comeback that you die.");
+        }
+        else if(in == 2)
+        {
+          System.out.println("He says \"Yeah, let's be friends!\" You both walk away from the pool holding hands and frolicked into the sunset.");
+        }
+        else if(in == 3)
+        {
+          System.out.println("He says \"Hmmm. What's in it for me?\"");
+          System.out.println("1 - \"The corndog allows you to harness dark magic.\"");
+          System.out.println("2 - \"The corndog gives you super strength.\"");
+          System.out.println("3 - \"The corndog tastes good.\"");
+          in = scan.nextInt();
+          if(in == 1)
+          {
+            System.out.println("He says \"Oh boy, Gimme that.\" He eats the corndog and then dies.");
+          }
+          else if(in == 2)
+          {
+            System.out.println("He says \"Okay I'll try it.\"He eats the corndog and then his arms disappear. Coach kicks both of you off the team.");
+          }
+          else if(in == 3)
+          {
+            System.out.println("He says \"Alright.\" He eats the corndog and nothing happens. You miss your event while this is happening. Coach kicks you off the team.");
+          }
+        }
+      }
+      else if(in == 2)
+      {
+        System.out.println("He punches u in the stomach so hard that you go flying 25 meters over the pool to the other side");
+      }
+      else if(in == 3)
+      {
+        System.out.println("He starts crying. The coach sees you and kicks you off the team.");
+      }
+    }                  
   }
 }
     
